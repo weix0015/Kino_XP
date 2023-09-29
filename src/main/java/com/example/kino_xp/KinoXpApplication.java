cbe32efe919a6e659e8290b49dc325741bc18423
weix0015 <weix0015@stud.kea.dk>
2023-09-29 11:50:48 +0200

    Init Commit

---
+++ src/main/java/com/example/kino_xp/KinoXpApplication.java
@@ -0,0 +1,13 @@
+package com.example.kino_xp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KinoXpApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/kino_xp/KinoXpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kino_xp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
