649e57b93a6c68f7f764e60dcd59b2de46dab9dc
Lasse Hallingskov <lass347s@stud.kea.dk>
2023-10-04 11:06:32 +0200

    Made changes to viewing.java.

--- src/main/java/com/example/kino_xp/model/Viewing.java
+++ src/main/java/com/example/kino_xp/model/Viewing.java
@@ -1,5 +1,64 @@
 package com.example.kino_xp.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
 public class Viewing {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private LocalDateTime showTime;
+    private int hall;
+    private LocalDateTime showEndTime;
+
+    //Constructors
+    public Viewing() {
+
+
+    }
+
+    public Viewing(int id, LocalDateTime showTime, int hall, LocalDateTime showEndTime) {
+        this.id = id;
+        this.showTime = showTime;
+        this.hall = hall;
+        this.showEndTime = showEndTime;
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(LocalDateTime showTime) {
+        this.showTime = showTime;
+    }
+
+    public int getHall() {
+        return hall;
+    }
+
+    public void setHall(int hall) {
+        this.hall = hall;
+    }
+
+    public LocalDateTime getShowEndTime() {
+        return showEndTime;
+    }
+
+    public void setShowEndTime(LocalDateTime showEndTime) {
+        this.showEndTime = showEndTime;
+    }
 }
