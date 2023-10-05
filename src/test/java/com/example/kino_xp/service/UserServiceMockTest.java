e477b33ed899b518a1a2ed45e50c56d29ca8f9d5
Isaac Levy <isaa0267@stud.kea.dk>
2023-10-05 14:02:53 +0200

    the new code

--- src/test/java/com/example/kino_xp/service/UserServiceMockTest.java
+++ src/test/java/com/example/kino_xp/service/UserServiceMockTest.java
@@ -22,148 +22,148 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UserServiceMockTest {
 
-  @Mock
-  private UserRepository mockedUserRepository;
+    @Mock
+    private UserRepository mockedUserRepository;
 
-  @Autowired
-  UserConverter userConverter;
+    @Autowired
+    UserConverter userConverter;
 
-  UserService userService;
-
-
-  User userToSave = new User(
-    3,
-    "Peter",
-    "Peter@mail.com",
-    "somecoolpassword",
-    null,
-    false
-  );
+    UserService userService;
 
 
-  //ARRANGE
-  @BeforeEach
-  void init() {
-    userService = new UserService(mockedUserRepository, userConverter);
-    User user1 = new User();
-    user1.setId(1);
-    user1.setName("Mikkel");
-    user1.setEmail("mikkel@email.com");
-    user1.setPassword("WhatACoolPassword");
-    user1.setTickets(null);
-    user1.setAdmin(false);
+    User userToSave = new User(
+            3,
+            "Peter",
+            "Peter@mail.com",
+            "somecoolpassword",
+            null,
+            false
+    );
 
-    User user2 = new User();
-    user2.setId(2);
-    user2.setName("Martin");
-    user2.setEmail("martin@email.com");
-    user2.setPassword("YouKnowWhatThatsAPrettyNeatPassword");
-    user2.setTickets(null);
-    user2.setAdmin(true);
 
-    List<User> userList = new ArrayList<>();
-    userList.add(user1);
-    userList.add(user2);
+    //ARRANGE
+    @BeforeEach
+    void init(){
+        userService = new UserService(mockedUserRepository, userConverter);
+        User user1 = new User();
+        user1.setId(1);
+        user1.setName("Mikkel");
+        user1.setEmail("mikkel@email.com");
+        user1.setPassword("WhatACoolPassword");
+        user1.setTickets(null);
+        user1.setAdmin(false);
 
-    //MOCK SETUP
+        User user2 = new User();
+        user2.setId(2);
+        user2.setName("Martin");
+        user2.setEmail("martin@email.com");
+        user2.setPassword("YouKnowWhatThatsAPrettyNeatPassword");
+        user2.setTickets(null);
+        user2.setAdmin(true);
 
-    //GetAllUsers
-    Mockito.when(mockedUserRepository.findAll()).thenReturn(userList);
+        List<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
 
-    //GetUserById
-    Mockito.when(mockedUserRepository.findById(1)).thenReturn(Optional.of(user1));
-    Mockito.when(mockedUserRepository.findById(2)).thenReturn(Optional.of(user2));
-    Mockito.when(mockedUserRepository.findById(42)).thenReturn(Optional.empty());
+        //MOCK SETUP
 
-    //GetUsersByName
-    Mockito.when(mockedUserRepository.findAllByName("Mikkel")).thenReturn(new ArrayList<>(Arrays.asList(user1)));
-    Mockito.when(mockedUserRepository.findAllByName("Martin")).thenReturn(new ArrayList<>(Arrays.asList(user2)));
+        //GetAllUsers
+        Mockito.when(mockedUserRepository.findAll()).thenReturn(userList);
 
-    //GetUsersByEmail
-    Mockito.when(mockedUserRepository.findAllByEmail("mikkel@email.com")).thenReturn(new ArrayList<>(Arrays.asList(user1)));
-    Mockito.when(mockedUserRepository.findAllByEmail("martin@email.com")).thenReturn(new ArrayList<>(Arrays.asList(user2)));
+        //GetUserById
+        Mockito.when(mockedUserRepository.findById(1)).thenReturn(Optional.of(user1));
+        Mockito.when(mockedUserRepository.findById(2)).thenReturn(Optional.of(user2));
+        Mockito.when(mockedUserRepository.findById(42)).thenReturn(Optional.empty());
 
-    //Create/Update User
-    Mockito.when(mockedUserRepository.save(Mockito.any(User.class))).thenReturn(userToSave);
+        //GetUsersByName
+        Mockito.when(mockedUserRepository.findAllByName("Mikkel")).thenReturn(new ArrayList<>(Arrays.asList(user1)));
+        Mockito.when(mockedUserRepository.findAllByName("Martin")).thenReturn(new ArrayList<>(Arrays.asList(user2)));
 
-    //DeleteUserById
-    Mockito.doNothing().when(mockedUserRepository).deleteById(1);
-  }
+        //GetUsersByEmail
+        Mockito.when(mockedUserRepository.findAllByEmail("mikkel@email.com")).thenReturn(new ArrayList<>(Arrays.asList(user1)));
+        Mockito.when(mockedUserRepository.findAllByEmail("martin@email.com")).thenReturn(new ArrayList<>(Arrays.asList(user2)));
 
-  @Test
-  void getAllUsers() {
-    //ACT
-    List<UserDTO> userDTOList = userService.getAllUsers();
+        //Create/Update User
+        Mockito.when(mockedUserRepository.save(Mockito.any(User.class))).thenReturn(userToSave);
 
-    //ASSERT
-    assertEquals("Mikkel", userDTOList.get(0).name());
-    assertEquals("Martin", userDTOList.get(1).name());
-  }
+        //DeleteUserById
+        Mockito.doNothing().when(mockedUserRepository).deleteById(1);
+    }
 
-  @Test
-  void getUserById() {
-    //ACT
-    UserDTO userDTO1 = userService.getUserById(1);
-    UserDTO userDTO2 = userService.getUserById(2);
+    @Test
+    void getAllUsers() {
+        //ACT
+        List<UserDTO> userDTOList = userService.getAllUsers();
 
-    //ASSERT
-    assertEquals("Mikkel", userDTO1.name());
-    assertEquals("Martin", userDTO2.name());
+        //ASSERT
+        assertEquals("Mikkel", userDTOList.get(0).name());
+        assertEquals("Martin", userDTOList.get(1).name());
+    }
 
-    assertThrows(UserNotFoundException.class, () -> userService.getUserById(42));
+    @Test
+    void getUserById() {
+        //ACT
+        UserDTO userDTO1 = userService.getUserById(1);
+        UserDTO userDTO2 = userService.getUserById(2);
 
-  }
+        //ASSERT
+        assertEquals("Mikkel", userDTO1.name());
+        assertEquals("Martin", userDTO2.name());
 
-  @Test
-  void getUsersByEmail() {
-    //ACT
-    UserDTO userDTO1 = userService.getUserByEmail("mikkel@email.com");
-    UserDTO userDTO2 = userService.getUserByEmail("martin@email.com");
+        assertThrows(UserNotFoundException.class, () -> userService.getUserById(42));
 
-    //ASSERT
-    assertEquals("Mikkel", userDTO1.name());
-    assertEquals("Martin", userDTO2.name());
+    }
 
-    assertThrows(UserNotFoundException.class, () -> userService.getUserByEmail("doesNotExist@email.com"));
-  }
+    @Test
+    void getUsersByEmail() {
+        //ACT
+        UserDTO userDTO1 = userService.getUserByEmail("mikkel@email.com");
+        UserDTO userDTO2 = userService.getUserByEmail("martin@email.com");
 
-  @Test
-  void getUserByName() {
-    //ACT
-    UserDTO u1 = userService.getUsersByName("Mikkel").get(0);
-    UserDTO u2 = userService.getUsersByName("Martin").get(0);
+        //ASSERT
+        assertEquals("Mikkel", userDTO1.name());
+        assertEquals("Martin", userDTO2.name());
 
-    //ASSERT
-    assertEquals("Mikkel", u1.name());
-    assertEquals("Martin", u2.name());
+        assertThrows(UserNotFoundException.class, () -> userService.getUserByEmail("doesNotExist@email.com"));
+    }
 
-    assertThrows(UserNotFoundException.class, () -> userService.getUsersByName("Bjarne"));
-  }
+    @Test
+    void getUserByName() {
+        //ACT
+        UserDTO u1 = userService.getUsersByName("Mikkel").get(0);
+        UserDTO u2 = userService.getUsersByName("Martin").get(0);
 
-  @Test
-  void createUser() {
-    //ACT
-    UserDTO userDTO = userService.createUser(userConverter.toDTO(userToSave));
+        //ASSERT
+        assertEquals("Mikkel", u1.name());
+        assertEquals("Martin", u2.name());
 
-    //ASSERT
-    assertEquals(userConverter.toDTO(userToSave), userDTO);
-  }
+        assertThrows(UserNotFoundException.class, () -> userService.getUsersByName("Bjarne"));
+    }
 
-  @Test
-  void updateUser() {
-    //ACT
-    UserDTO userDTO = userService.updateUser(1, userConverter.toDTO(userToSave));
+    @Test
+    void createUser() {
+        //ACT
+        UserDTO userDTO = userService.createUser(userConverter.toDTO(userToSave));
 
-    //ASSERT
-    assertEquals(userConverter.toDTO(userToSave), userDTO);
-  }
+        //ASSERT
+        assertEquals(userConverter.toDTO(userToSave), userDTO);
+    }
 
-  @Test
-  void deleteUserById() {
-    //ACT
-    userService.deleteUserById(1);
+    @Test
+    void updateUser() {
+        //ACT
+        UserDTO userDTO = userService.updateUser(1, userConverter.toDTO(userToSave));
 
-    //ASSERT
-    Mockito.verify(mockedUserRepository).deleteById(1);
-  }
+        //ASSERT
+        assertEquals(userConverter.toDTO(userToSave), userDTO);
+    }
+
+    @Test
+    void deleteUserById() {
+        //ACT
+        userService.deleteUserById(1);
+
+        //ASSERT
+        Mockito.verify(mockedUserRepository).deleteById(1);
+    }
 }
