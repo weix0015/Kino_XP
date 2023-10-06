54ae0e8f3af607c702fe98e0005df912b563b0ec
weix0015 <weix0015@stud.kea.dk>
2023-10-06 09:48:59 +0200

    Movie service test done

--- src/test/java/com/example/kino_xp/service/MovieServiceTest.java
+++ src/test/java/com/example/kino_xp/service/MovieServiceTest.java
@@ -4,6 +4,8 @@ import com.example.kino_xp.converter.MovieConverter;
 import com.example.kino_xp.dto.MovieDTO;
 import com.example.kino_xp.exception.MovieNotFoundException;
 import com.example.kino_xp.model.Genre;
+import com.example.kino_xp.model.Movie;
+import com.example.kino_xp.repository.MovieRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +24,9 @@ class MovieServiceTest {
 
     @Autowired
     MovieConverter movieConverter;
+
+    @Autowired
+    MovieRepository movieRepository;
 
     @Test
     void getAllTheMovie() {
@@ -48,7 +53,8 @@ class MovieServiceTest {
                 "The java",
                 Genre.ACTION,
                 LocalTime.of(3, 30, 30),
-                22);
+                22
+        );
         // Act
         MovieDTO createdMovieDTO = movieService.createMovie(movieDTO);
         // assert
@@ -64,13 +70,67 @@ class MovieServiceTest {
 
     @Test
     void updateMovie() {
+        // Arrange
+        MovieDTO movieDTO = new MovieDTO(
+          1,
+          "World",
+          Genre.ADVENTURE,
+          LocalTime.of(1, 30, 30),
+          18
+        );
+        // Act
+        MovieDTO updatedMovieDTO = movieService.updateMovie(1, movieDTO);
+
+        // Assert
+        assertNotNull(updatedMovieDTO);
+        assertEquals("World", updatedMovieDTO.title());
+        assertEquals(Genre.ADVENTURE, updatedMovieDTO.genre());
+        assertEquals(LocalTime.of(1, 30, 30), updatedMovieDTO.showLength());
+        assertEquals(18, updatedMovieDTO.age());
     }
 
     @Test
     void deleteMovie() {
+        // Arrange
+        Movie movieToDelete = new Movie();
+        movieToDelete.setTitle("Barbie");
+        movieToDelete.setGenre(Genre.COMEDY);
+        movieToDelete.setShowLength(LocalTime.of(2, 30, 30));
+        movieToDelete.setAge(18);
+        movieRepository.save(movieToDelete);
+
+        // Act
+        movieService.deleteMovie(movieToDelete.getId());
+
+        // Assert
+        assertFalse(movieRepository.existsById(movieToDelete.getId()));
     }
 
     @Test
     void getAllMoviesByTitle() {
+        // Arrange
+        Movie movie1 = new Movie();
+        movie1.setTitle("Batman");
+        movie1.setGenre(Genre.ACTION);
+        movie1.setShowLength(LocalTime.of(2, 0, 0));
+        movie1.setAge(18);
+        movieRepository.save(movie1);
+
+        Movie movie2 = new Movie();
+        movie2.setTitle("Hero");
+        movie2.setGenre(Genre.ACTION);
+        movie2.setShowLength(LocalTime.of(2, 10, 5));
+        movie2.setAge(18);
+        movieRepository.save(movie2);
+
+        // Act
+        List<MovieDTO> movies1 = movieService.getAllMoviesByTitle("Batman");
+        List<MovieDTO> movies2 = movieService.getAllMoviesByTitle("Hero");
+
+        // Assert
+        assertEquals(1, movies1.size());
+        assertEquals(1, movies2.size());
+        assertEquals("Batman", movies1.get(0).title());
+        assertEquals("Hero", movies2.get(0).title());
     }
 }
